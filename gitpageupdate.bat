39e28c9a40c010b4272f889e7eeb41ac7907ba0c
Chang-ji <75286090+Chang-ji@users.noreply.github.com>
2021-08-23 09:01:34 +0900

    second commit

---
+++ gitpageupdate.bat
@@ -0,0 +1,18 @@
+cd %~dp0
+cd output
+
+git add .
+timeout 1
+
+git commit -m "second commit"
+timeout 1
+
+git pull origin main
+timeout 1
+cd ..
+bundle exec jekyll serve
+timeout 1
+
+cd output
+git push -u origin main
+timeout 10
